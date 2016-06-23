39c8ed0b7fde75e6847a940dcad9b091e4be5c3d
ashik <ashik@ashikuzzaman>
2016-06-23 23:10:02 +0600

    Added ServiceProvider.java interface for providing SessionFactory and BeanProvider objects. Any class or controllers can implement this interface to get support for SessionFactory and BeanProvider classes.

---
+++ src/java/com/studevs/utils/providers/ServiceProvider.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.studevs.utils.providers;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+/**
+ *
+ * @author ashik
+ */
+public interface ServiceProvider {
+    
+    public static final SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+    public BeanProvider beanProvider = new BeanProvider();
+}
