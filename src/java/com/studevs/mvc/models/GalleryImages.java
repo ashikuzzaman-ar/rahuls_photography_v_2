773e4d40ec7657230ff52437e904a7286c44139a
ashik <ashik@ashikuzzaman>
2016-06-25 04:43:03 +0600

    Added GalleryImages.java model for storing all portfolio images for gallery view.

---
+++ src/java/com/studevs/mvc/models/GalleryImages.java
@@ -0,0 +1,87 @@
+package com.studevs.mvc.models;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author ashik
+ */
+@Entity
+@Table(name = "GALLERY_IMAGES")
+public class GalleryImages implements Serializable{
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id ;
+    
+    @Column(name = "IMAGE")
+    private byte image[];
+    
+    @Column(name = "CATEGORY")
+    private String category ;
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Arrays.hashCode(this.image);
+        hash = 29 * hash + Objects.hashCode(this.category);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GalleryImages other = (GalleryImages) obj;
+        if (!Objects.equals(this.category, other.category)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return Arrays.equals(this.image, other.image);
+    }
+
+    public GalleryImages() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
